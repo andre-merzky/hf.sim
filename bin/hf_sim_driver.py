f1d7cc2f6f3164b7fbf7f57a63442d57d004eebf
Andre Merzky <andre@merzky.net>
2017-11-27 12:56:43 +0100

    parameter update

--- bin/hf_sim_driver.py
+++ bin/hf_sim_driver.py
@@ -10,37 +10,37 @@ flat   = sim.create_flat_distribution
 rep    = ru.LogReporter(name='hf')
 
 
-cfg = { 'farmer'   : { 'sprout'   : { 'min'  :  100,
-                                      'max'  :  250,
-                                      'mean' :  150,
-                                      'var'  :    5
-                                     },
-                       'length'   : { 'min'  : 2000,
-                                      'max'  : 3000,
-                                      'mean' : 2750,
-                                      'var'  :    5
-                                     },
-                       'diameter' : { 'min'  : 4,
-                                      'max'  : 15,
-                                     },
+cfg = { 'farmer'   : {'sprout'          : {'min'  :   50,
+                                           'max'  :  100,
+                                           'mean' :   75,
+                                           'var'  :    5
+                                          },
+                      'length'          : {'min'  : 1000,
+                                           'max'  : 2500,
+                                           'mean' : 2000,
+                                           'var'  :    5
+                                          },
+                      'diameter'        : {'min'  :    4,
+                                           'max'  :   15,
+                                          },
                      },
-        'peeler'   : { 'min_len'         :   300,
-                       'max_len'         :  1600, 
-                       'min_dia'         :     6,
-                       'max_dia'         :    12,
-                       'prep_efficiency' :    99, # in percent
-                       'peel_efficiency' :    90, # in percent
+        'peeler'   : {'min_len'         :   300,
+                      'max_len'         :  1600, 
+                      'min_dia'         :     6,
+                      'max_dia'         :    12,
+                      'prep_efficiency' :    99,  # in percent
+                      'peel_efficiency' :    90,  # in percent
 
-                       'success_min'     :     0, # in percent of stalk length
-                       'success_max'     :   100,
-                       'success_mean'    :    90,
-                       'success_var'     :     1,
+                      'success_min'     :     0,  # in percent of stalk length
+                      'success_max'     :   100,
+                      'success_mean'    :    90,
+                      'success_var'     :     1,
                      },
-        'stitcher' : { 'resolution'      :   10,
-                       'splice_width'    :    7,
-                       'seg_width'       :   12,
-                       'seg_length'      : 1000, 
-                       'mode'            : 'continuous'
+        'stitcher' : {'resolution'      :    10,
+                      'splice_width'    :     7,
+                      'seg_width'       :    12,
+                      'seg_length'      :   500, 
+                      'mode'            :  'continuous'
                      },
       }
 
